5f2a655654dab2c727790514dfcdaa7a173f2bf0
= <jperaza@oktopus-hn.com>
2017-08-21 18:15:22 -0600

    se agrego firebase a el portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -16,6 +16,7 @@ export class ProductosService {
     this.http.get("https://portafolio-3e190.firebaseio.com/productos_idx.json")
     .subscribe( res => {
       console.log(res.json());
+      this.productos = res.json();
       this.cargando = false;
     })
    }
